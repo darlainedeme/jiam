b61824bed56d1615f7bcdf43db2123d4ddd469a4
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2022-08-04 17:50:23 +0200

    Add files via upload

---
+++ planning.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  4 16:36:58 2022
+
+@author: EDEME_D
+"""
+
+import os
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+from streamlit_folium import folium_static
+import folium
+import geopandas as gpd
+
+
+places = pd.read_csv('places.csv', encoding="ISO-8859-1", index_col=0)
+places = places[places['Latitude'].notna()]
+places = gpd.GeoDataFrame(places, geometry=gpd.points_from_xy(places['Longitude'], places['Latitude']))
+# center on Liberty Bell
+long = places.geometry.x.mean()
+lat = places.geometry.y.mean()
+            
+          
+m = folium.Map(location=[lat, long], zoom_start=9)
+tile = folium.TileLayer(
+    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    attr='Esri',
+    name='Esri Satellite',
+    overlay=False,
+    control=True
+).add_to(m)
+
+tile = folium.TileLayer(
+    tiles='http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}',
+    attr='Google',
+    name='Google Hybrid',
+    overlay=False,
+    control=True
+).add_to(m)
+
+tile = folium.TileLayer(
+    tiles='http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}',
+    attr='Google',
+    name='Google Maps',
+    overlay=False,
+    control=True
+).add_to(m)
+
+
+feature_group_1 = folium.FeatureGroup(name='Case', show=True)
+feature_group_2 = folium.FeatureGroup(name='Negril', show=True)
+feature_group_3 = folium.FeatureGroup(name='Port Antonio', show=True)
+feature_group_4 = folium.FeatureGroup(name='St. Ann Parish', show=True)
+
+style1 = {'fillColor': 'green', 'color': 'green'}
+style2 = {'fillColor': 'yellow', 'color': 'yellow'}
+style3 = {'fillColor': 'blue', 'color': 'blue'}
+style4 = {'fillColor': 'red', 'color': 'red'}            
+
+def plotcase(point):
+    '''input: series that contains a numeric named latitude and a numeric named longitude
+    this function creates a CircleMarker and adds it to your this_map'''
+    folium.CircleMarker(location=[point.Latitude, point.Longitude],
+                        popup=str(point.Dove) 
+                        + "\n" 
+                        + "\n" 
+                        + str(point.Nome)
+                        + "\n" 
+                        + "\n" 
+                        + str(point.Descrizione),
+                        color = 'green',
+                        radius=2,
+                        weight=5).add_to(feature_group_1)
+
+def plotnegril(point):
+    '''input: series that contains a numeric named latitude and a numeric named longitude
+    this function creates a CircleMarker and adds it to your this_map'''
+    folium.CircleMarker(location=[point.Latitude, point.Longitude],
+                        popup=str(point.Dove) 
+                        + "\n" 
+                        + "\n" 
+                        + str(point.Nome)
+                        + "\n" 
+                        + "\n" 
+                        + str(point.Descrizione),
+                        color = 'red',
+                        radius=2,
+                        weight=5).add_to(feature_group_2)
+
+def plotantonio(point):
+    '''input: series that contains a numeric named latitude and a numeric named longitude
+    this function creates a CircleMarker and adds it to your this_map'''
+    folium.CircleMarker(location=[point.Latitude, point.Longitude],
+                        popup=str(point.Dove) 
+                        + "\n" 
+                        + "\n" 
+                        + str(point.Nome)
+                        + "\n" 
+                        + "\n" 
+                        + str(point.Descrizione),
+                        color = 'blue',
+                        radius=2,
+                        weight=5).add_to(feature_group_3)
+
+def plotanna(point):
+    '''input: series that contains a numeric named latitude and a numeric named longitude
+    this function creates a CircleMarker and adds it to your this_map'''
+    folium.CircleMarker(location=[point.Latitude, point.Longitude],
+                        popup=str(point.Dove) 
+                        + "\n" 
+                        + "\n" 
+                        + str(point.Nome)
+                        + "\n" 
+                        + "\n" 
+                        + str(point.Descrizione),
+                        color = 'yellow',
+                        radius=2,
+                        weight=5).add_to(feature_group_4)
+
+
+# case
+case_gdf = places[places.Cosa == 'CASA']
+case_gdf.apply(plotcase, axis = 1)
+
+negril_gdf = places[(places.Dove == 'NEGRIL') & (places.Cosa != 'CASA')]
+negril_gdf.apply(plotnegril, axis = 1)
+
+anthony_gdf = places[(places.Dove == 'PORT ANTONIO') & (places.Cosa != 'CASA')]
+anthony_gdf.apply(plotantonio, axis = 1)
+
+anna_gdf = places[(places.Dove == 'ST. ANN PARISH') & (places.Cosa != 'CASA')]
+anna_gdf.apply(plotanna, axis = 1)
+
+
+feature_group_1.add_to(m)
+feature_group_2.add_to(m)
+feature_group_3.add_to(m)
+feature_group_4.add_to(m)
+
+
+m.fit_bounds(m.get_bounds())
+           
+                    
+folium.plugins.Draw(export=True, filename='data.geojson', position='topleft', draw_options=None,
+                    edit_options=None).add_to(m)
+folium.plugins.Fullscreen(position='topleft', title='Full Screen', title_cancel='Exit Full Screen',
+                          force_separate_button=False).add_to(m)
+folium.plugins.MeasureControl(position='bottomleft', primary_length_unit='meters', secondary_length_unit='miles',
+                              primary_area_unit='sqmeters', secondary_area_unit='acres').add_to(m)
+folium.LayerControl().add_to(m)
+
+
+
+# Displaying a map         
+m.save('map.html')
+folium_static(m)
+
+
+            
